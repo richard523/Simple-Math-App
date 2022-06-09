474ac32d5b135bfa7968bb43fc8779b20baa3005
richard523 <60868589+richard523@users.noreply.github.com>
2022-06-09 10:15:44 -0500

    Finished Math App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter.jsx';
+import Fib from './Fib.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>A Simple Math App</h1>
+      <Counter/> 
+      <Fib/>
       </header>
     </div>
   );
